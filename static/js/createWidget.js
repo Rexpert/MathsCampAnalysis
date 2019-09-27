fb5eddad59a9529b2af41509a8064bb2a85c4568
Andy Tan <andytan0727@gmail.com>
2019-09-27 12:07:14 +0800

    using markdown for writing/editing pages

--- static/js/createWidget.js
+++ static/js/createWidget.js
@@ -1,24 +1,47 @@
-const createResponsiveEmbed = url => {
-  const div = document.createElement("div");
-  const iframe = document.createElement("iframe");
-  div.classList.add("embed-responsive", "embed-responsive-16by9");
+/**
+ * Parse HTML doc from markdown files in /static/markdown, then
+ * attach the parsed markdown to HTML div
+ *
+ * @param {string} path path to markdown file
+ * @returns div populated with parsed markdown
+ */
+const generateHTMLFromMarkdown = async path => {
+  const markedDiv = document.createElement("div");
+  const res = await fetch(path);
+  const data = await res.text();
 
-  iframe.src = url;
+  markedDiv.innerHTML = marked(data);
+
+  return markedDiv;
+};
+
+/**
+ * Let iframe responsive by using bootstrap 4 embed responsive classes.
+ * Attach responsive div as iframe parent
+ *
+ * @param {HTMLIFrameElement} iframe iframe element
+ */
+const generateResponsiveIframe = iframe => {
+  const parent = iframe.parentNode;
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("embed-responsive", "embed-responsive-16by9");
   iframe.classList.add("embed-responsive-item");
-  div.appendChild(iframe);
-  return div;
+
+  parent.replaceChild(wrapper, iframe);
+  wrapper.appendChild(iframe);
 };
 
 window.addEventListener("load", async () => {
-  const scoresChartDiv = document.querySelector("#scores-chart");
-  const moneyAllocationChartDiv = document.querySelector(
-    "#money-allocation-chart"
-  );
-  const scoresResponsiveDiv = createResponsiveEmbed("./static/scores.html");
-  const moneyAllocationResponsiveDiv = createResponsiveEmbed(
-    "./static/MoneyAllocation.html"
+  const homeMainElem = document.querySelector("#home-page-main");
+  const markedDiv = await generateHTMLFromMarkdown(
+    "./static/markdown/homepage.md"
   );
 
-  scoresChartDiv.appendChild(scoresResponsiveDiv);
-  moneyAllocationChartDiv.appendChild(moneyAllocationResponsiveDiv);
+  // attach parsed markdown to home page main content
+  homeMainElem.appendChild(markedDiv);
+
+  const iframes = document.querySelectorAll("iframe");
+  for (const iframe of iframes) {
+    generateResponsiveIframe(iframe);
+  }
 });
