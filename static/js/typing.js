9a211b771653b2c2680822f220d79a572bf4126d
Andy Tan <andytan0727@gmail.com>
2019-10-07 21:16:06 +0800

    Update page title styles
    - Change font
    - Change representation (using typing)

---
+++ static/js/typing.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+  var options = {
+    strings: ["Maths Camp UUM 2019", "Maths Camp Analysis"],
+    typeSpeed: 70,
+    backSpeed: 50,
+  };
+
+  var typed = new Typed(".navbar-brand", options);
+});
